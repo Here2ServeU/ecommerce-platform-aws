6d2e6d5898b562c594c813d9e92e2ac7d951a9bc
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-19 15:52:57 -0600

    Create variables.tf

---
+++ modules/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "user_pool_name" {
+  description = "The name of the Cognito User Pool"
+  type        = string
+}
+
+variable "identity_pool_name" {
+  description = "The name of the Cognito Identity Pool"
+  type        = string
+}
