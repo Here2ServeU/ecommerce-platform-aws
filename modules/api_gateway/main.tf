f4fed31e1dd2e824d8fe9415d95bf623f22bd3ae
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-21 17:38:13 -0600

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,26 +1,38 @@
-resource "aws_apigatewayv2_api" "api" {
-  name          = var.api_name
-  protocol_type = "HTTP"
+# API Gateway REST API
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "${var.api_name}-${var.environment}"
+  description = "API for the ${var.environment} environment of the T2S E-commerce Platform"
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = var.lambda_function_arn
-  payload_format_version = "2.0"
+# Resource for the `/enroll` route
+resource "aws_api_gateway_resource" "enroll" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "enroll"
 }
 
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /enroll"
-
-  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+# POST Method for the `/enroll` route
+resource "aws_api_gateway_method" "post_enroll" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.enroll.id
+  http_method   = "POST"
+  authorization = "NONE" # Change to AWS_IAM or CUSTOM if using authentication
 }
 
-resource "aws_lambda_permission" "allow_apigateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_arn
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+# Integration with Lambda for the POST `/enroll` method
+resource "aws_api_gateway_integration" "post_enroll" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.enroll.id
+  http_method             = aws_api_gateway_method.post_enroll.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.backend.invoke_arn
 }
+
+# Deployment for the API
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.post_enroll]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = var.environment
+}
+
