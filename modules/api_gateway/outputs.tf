09c197c2afc5ade0b7ddfa18f9df3c6d6c3f1e87
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-21 17:38:43 -0600

    Update outputs.tf

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -1,3 +1,12 @@
 output "api_endpoint" {
   value = aws_apigatewayv2_api.api.api_endpoint
 }
+
+# Outputs
+output "api_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "invoke_url" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}/${var.environment}"
+}
