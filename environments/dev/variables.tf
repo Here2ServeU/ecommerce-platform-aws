81c9baa5d9a92ff702c26b30496d37b850d181af
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-12-26 16:25:04 -0600

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,150 +1,47 @@
-# Terraform Backend Variables
-variable "backend_bucket_name" {
-  description = "Name of the S3 bucket used for the backend state"
-  type        = string
-}
+# Backend
+backend_bucket_name    = "t2s-ecommerce-tf-state"
+backend_key            = "ecommerce/terraform.tfstate"
+backend_region         = "us-east-1"
+backend_dynamodb_table = "t2s-terraform-locks"
+backend_encrypt        = true
 
-variable "backend_key" {
-  description = "Path to the backend state file"
-  type        = string
-}
+# General
+aws_region             = "us-east-1"
+environment            = "dev"
 
-variable "backend_region" {
-  description = "AWS region for the backend"
-  type        = string
-}
+# S3
+bucket_name            = "t2s-dev-frontend"
 
-variable "backend_dynamodb_table" {
-  description = "DynamoDB table for state locking"
-  type        = string
-}
+# DynamoDB
+dynamodb_table_name    = "t2s-dev-table"
 
-variable "backend_encrypt" {
-  description = "Whether to enable encryption for the backend"
-  type        = bool
-}
+# RDS
+rds_allocated_storage  = 20
+rds_engine             = "mysql"
+rds_engine_version     = "8.0"
+rds_instance_class     = "db.t3.micro"
+rds_db_name            = "t2s_dev_db"
+rds_username           = "admin"
+rds_password           = "securepassword"
+rds_publicly_accessible = false
 
-# General Variables
-variable "aws_region" {
-  description = "AWS region for the deployment"
-  type        = string
-}
+# SNS
+sns_topic_name         = "t2s-dev-sns-topic"
 
-variable "environment" {
-  description = "Deployment environment (e.g., dev, stage, prod)"
-  type        = string
-}
+# Lambda
+lambda_function_name   = "t2s-backend-dev-function"
+lambda_source_dir      = "../lambda_code"
+lambda_runtime         = "python3.8"
+lambda_filename        = "lambda_function.zip"
+lambda_handler         = "app.handler"
+lambda_role_name       = "lambda-execution-role"
 
-# S3 Variables
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
+# IAM
+role_name              = "t2s-iam-role"
 
-# DynamoDB Variables
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table"
-  type        = string
-}
+# Cognito
+cognito_user_pool_name = "t2s-user-pool-dev"
+cognito_identity_pool_name = "t2s-identity-pool-dev"
 
-# RDS Variables
-variable "rds_allocated_storage" {
-  description = "Allocated storage size for the RDS instance"
-  type        = number
-}
-
-variable "rds_engine" {
-  description = "Database engine type for RDS (e.g., mysql, postgres)"
-  type        = string
-}
-
-variable "rds_engine_version" {
-  description = "Database engine version for RDS"
-  type        = string
-}
-
-variable "rds_instance_class" {
-  description = "Instance class for the RDS database"
-  type        = string
-}
-
-variable "rds_db_name" {
-  description = "Name of the RDS database"
-  type        = string
-}
-
-variable "rds_username" {
-  description = "Username for the RDS database"
-  type        = string
-}
-
-variable "rds_password" {
-  description = "Password for the RDS database"
-  type        = string
-  sensitive   = true
-}
-
-variable "rds_publicly_accessible" {
-  description = "Whether the RDS instance should be publicly accessible"
-  type        = bool
-}
-
-# SNS Variables
-variable "sns_topic_name" {
-  description = "Name of the SNS topic"
-  type        = string
-}
-
-# Lambda Variables
-variable "lambda_function_name" {
-  description = "Name of the Lambda function"
-  type        = string
-}
-
-variable "lambda_source_dir" {
-  description = "Path to the directory containing Lambda function code"
-  type        = string
-}
-
-variable "lambda_runtime" {
-  description = "Runtime environment for the Lambda function"
-  type        = string
-}
-
-variable "lambda_filename" {
-  description = "Name of the Lambda deployment package"
-  type        = string
-}
-
-variable "lambda_handler" {
-  description = "The entry point handler for the Lambda function"
-  type        = string
-}
-
-variable "lambda_role_name" {
-  description = "Name of the IAM role for the Lambda function"
-  type        = string
-}
-
-# IAM Variables
-variable "role_name" {
-  description = "Name of the IAM role"
-  type        = string
-}
-
-# Cognito Variables
-variable "cognito_user_pool_name" {
-  description = "Name of the Cognito User Pool"
-  type        = string
-}
-
-variable "cognito_identity_pool_name" {
-  description = "Name of the Cognito Identity Pool"
-  type        = string
-}
-
-# API Gateway Variables
-variable "api_name" {
-  description = "Name of the API Gateway"
-  type        = string
-}
+# API Gateway
+api_name               = "t2s-api"
